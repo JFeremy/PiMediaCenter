645c1bcdd4c11b509e281e550da349ffaaca55a1
Jérémy <fumeron.jeremy@live.fr>
2023-06-10 10:44:12 +0200

    fix(raspap): fix the path of the service

--- check_wifi.sh
+++ check_wifi.sh
@@ -7,5 +7,5 @@ if [[ $connected == "YOUR_SSID_WIFI" ]]; then
   echo "Connexion au réseau Wi-Fi '$connected' déjà établie. Aucune action requise."
 else
   echo "Connexion au réseau Wi-Fi '$connected' non détectée. Lancement du service RaspAP..."
-  sudo systemctl start raspap
+  sudo systemctl start raspapd.service
 fi
