cc875df23048f2934757a8ca39c0c007a58e4986
Jérémy <fumeron.jeremy@live.fr>
2023-07-14 16:28:40 +0200

    update

--- 0_setup_pi.sh
+++ 0_setup_pi.sh
@@ -11,8 +11,8 @@ sudo apt autoclean
 # Préparer les autorisations pour les scripts
 chmod +x 1_add_disque.sh
 chmod +x 2_setup_samba.sh
-chmod +x 3_install_tools.sh
-chmod +x check_wifi.sh
+chmod +x 3_install_jellyfin.sh
+chmod +x 4_install_raspap.sh
 
 # Redémarrage du Raspberry Pi
 sudo reboot
